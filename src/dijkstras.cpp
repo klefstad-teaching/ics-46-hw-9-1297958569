95bbe2bde6e391abffa726b90a9c014ab876ea84
Wenxuan Chen <1297958569@qq.com>
2025-03-15 14:07:03 +0000

    format fix

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -74,8 +74,8 @@ void print_path(const vector<int>& path, int total) {
     for (size_t i = 0; i < path.size(); i++) {
         cout << path[i];
         if (i < path.size() - 1) {
-            cout << " ";  
+            cout << " ";
         }
     }
-    cout << "\nTotal cost: " << total << endl;
+    cout << "\nTotal cost: " << total << "\n";
 }
