3535c57666aa3ebd997329f7f918ec0c674862f9
Wenxuan Chen <1297958569@qq.com>
2025-03-16 23:43:04 +0000

    bg

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -76,9 +76,7 @@ void print_path(const vector<int>& path, int total) {
     // Print path without "Path:" prefix
     for (size_t i = 0; i < path.size(); i++) {
         cout << path[i];
-        if (i < path.size() - 1) {
-            cout << " ";
-        }
+        cout << " ";  // Always print a space after each number, including the last one
     }
     cout << "\nTotal cost is " << total << "\n";
 }
