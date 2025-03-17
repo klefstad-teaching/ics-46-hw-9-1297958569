5de8f7b90786aa956b88a705c59346f9c619104f
Wenxuan Chen <1297958569@qq.com>
2025-03-17 04:27:23 +0000

    bg

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,20 +1,28 @@
 #include "dijkstras.h"
 
-int main() {
-    Graph G;
-    try {
-        file_to_graph("small.txt", G);
-    } catch (const runtime_error& e) {
-        cout << "Error: " << e.what() << endl;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <graph_file>" << endl;
         return 1;
     }
     
-    vector<int> previous;
-    vector<int> distances = dijkstra_shortest_path(G, 0, previous);
+    string filename = argv[1];
+    Graph G;
     
-    // 打印从源（0）到所有顶点的最短路径
-    for (int i = 0; i < G.numVertices; i++) {
-        cout << "\nShortest path from 0 to " << i << ":" << endl;
+    try {
+        file_to_graph(filename, G);
+    } catch (const exception& e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
+    
+  
+    int source = 0;
+    vector<int> previous;
+
+    vector<int> distances = dijkstra_shortest_path(G, source, previous);
+    
+    for (int i = 0; i < G.numVertices; ++i) {
         vector<int> path = extract_shortest_path(distances, previous, i);
         print_path(path, distances[i]);
     }
